b90dcbd2ec849a7e65c3997e7ab88d00e2b283de
MathusanKandiah <mathusankandiah@hotmail.com>
2021-09-09 08:22:43 +0100

    formatted the styles for the links with a dark bg and white text with no underlining, all correct links have been replaced with associated headlines

--- app.js
+++ app.js
@@ -2,32 +2,24 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 app.use(cors());
+const fullResults = require('./data')
 
 
-results = [
-    {headline:'Rasool has quit futureproof', main:"damn"},
-    {headline:'Arafath has quit futureproof', main:"damn"},
-    {headline:'Ria has quit futureproof', main:"damn"},
-    {headline:'Callum has quit futureproof', main:"damn"},
-    {headline:'Ben has quit futureproof', main:"damn"},
-    {headline:'Dim has quit futureproof', main:"damn"},
-    {headline:'Sansi has quit futureproof', main:"damn"},
-    {headline:'Brodie has quit futureproof', main:"damn"},
-    {headline:'Velin has quit futureproof', main:"damn"},
-    {headline:'Claire has quit futureproof', main:"damn"}
-]
 
 app.get('/', (req, res) => res.send('Hello World'))
 
 app.get('/results', (req, res) => {
-    const headlines = results.map(story => story.headline); 
-    res.send(headlines)
+    res.send(fullResults)
+    // const headlines = fullResults.map(story => story.headline); 
+    // res.send(headlines)
 
-}
+
+    
+    }
 )
 
 let port = 3000
-// To do: Get the server running
+
 app.listen(port, ()=> {
   console.log(`Server is up and running at localhost:${port}`)
 })
--- script.js
+++ script.js
@@ -3,14 +3,48 @@ const buttongs = document.getElementById('buttongs')
 buttongs.addEventListener('click',search)
 
 function search(e) {
-  e.preventDefault()
-  fetch('http://localhost:3000/results')
-  .then(resp => resp.text())
-    .then(resp => document.getElementById('googlesearch').innerHTML = resp)
-
-    
-
+    e.preventDefault()
+    fetch('http://localhost:3000/results')
+    .then(resp => resp.json())
+    .then(resp = createList)
 } 
+
+const createList = resp => {
+    let list = document.createElement('ul')
+    document.getElementById('googlesearches').appendChild(list)
+    list.setAttribute("class","list-group")
+    for(let i=0; i< resp.length; i++) {
+        
+        let listItem = document.createElement("li");            
+        let text = resp[i].headline
+        console.log(text)
+        let link = document.createElement("a") 
+        
+        list.appendChild(listItem)
+        listItem.append(link)
+        listItem.setAttribute("class","list-group-item text-center bg-dark")
+        link.id = `link${i}`
+        link.setAttribute("href",`${resp[i].hlink}`)
+        link.setAttribute("class","text-light")
+        link.setAttribute("style","text-decoration:none")
+
+        document.getElementById(`link${i}`).innerText = text   
+    }
+                              
+ }
+
+// const createList = (resp) => {
+//     for(let i=0; i< resp.length; i++) {
+//         let listItem = document.createElement("li");            
+//         let text = document.createTextNode(resp[i]);         
+//         listItem.appendChild(text);                           
+//         document.getElementById("newList").appendChild(listItem);   
+//     }
+                              
+//  }
+
+
+
 
 
 // const googlesearch = document.getElementById('googlesearch')
